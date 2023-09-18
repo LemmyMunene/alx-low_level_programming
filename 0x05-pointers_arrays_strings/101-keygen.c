87482b2ae2054201e3145095c829ba1530d901a8
LemmyMunene <lemmmwan@gmail.com>
2023-09-18 14:41:49 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+}
