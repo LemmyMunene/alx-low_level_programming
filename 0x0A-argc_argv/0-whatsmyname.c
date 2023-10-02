5463b24fb96eca442c3a24bb45454c4ad7855e62
LemmyMunene <lemmmwan@gmail.com>
2023-10-02 02:48:23 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints program name
+ * @argc: arguments
+ * @argv: name of Array
+ * Return: NULL
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
