a88840c74e4f4433387621c7a794787c46c6570f
LemmyMunene <lemmmwan@gmail.com>
2023-10-03 06:15:12 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@
 char *_strdup(char *str)
 {
 	  int i = 0, j, k = 0;
-	    char *newStr = 0;
+	    char *newStr = NULL;
 
 	      while (str[k] != '\0')
 		          {
@@ -22,8 +22,8 @@ char *_strdup(char *str)
 
 	        newStr = malloc(sizeof(char) * (i + 1));
 
-		  if (str == 0)
-			      return (0);
+		  if (str == NULL)
+			      return (NULL);
 
 		    for (j = 0; j <= i; j++)
 			        newStr[j] = str[j];
