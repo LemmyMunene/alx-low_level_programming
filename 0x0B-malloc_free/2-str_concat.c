ca69fc993b1cf7421a79b5565d0621c893eb6012
LemmyMunene <lemmmwan@gmail.com>
2023-10-03 06:38:24 -0700

    update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in mem.
+ * @str: string 1.
+ *
+ * Return: pointer to char array
+ */
+
+char *_strdup(char *str)
+{
+  int i = 0, j, k = 0;
+  char *newStr = 0;
+
+  while (str[k] != '\0')
+    {
+      i++;
+      k++;
+    }
+
+  newStr = malloc(sizeof(char) * (i + 1));
+
+  if (str == 0)
+    return (0);
+
+  for (j = 0; j <= i; j++)
+    newStr[j] = str[j];
+
+  return (newStr);
+}
