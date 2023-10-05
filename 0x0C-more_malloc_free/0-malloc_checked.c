678342b91042cc9faa95fa1f7a3ed5d6aa0cb2d5
LemmyMunene <lemmmwan@gmail.com>
2023-10-05 05:37:53 -0700

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - memory allocation.
- * @b: value passed.
- *
- * Return: NULL.
+ * malloc_checked - allocates memory using malloc
+ * @b: Value passed in for array creation
+ * Return: pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
